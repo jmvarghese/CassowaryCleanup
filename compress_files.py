d5d62e554e2b8853966109f5ed67778b9aa3af93
Nicholas Minor <nrminor@wisc.edu>
2023-11-07 12:30:15 -0600

    switching to text file based logging

--- compress_files.py
+++ compress_files.py
@@ -5,21 +5,32 @@
 
 import os
 import subprocess
-import logging
 import multiprocessing
 import argparse
-#from icecream import ic
 from typing import Tuple, Set, List
+from filelock import FileLock, Timeout
 
-def parse_command_line_args() -> str:
+
+def parse_command_line_args() -> Tuple[str, str]:
     """Parse command line arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("--search_dir", "-d",
-                        type=str,
-                        required=True,
-                        help="Directory to search for uncompressed/human readable sequence data.")
+    parser.add_argument(
+        "--search_dir",
+        "-d",
+        type=str,
+        required=True,
+        help="Directory to search for uncompressed/human readable sequence data.",
+    )
+    parser.add_argument(
+        "--logging_file",
+        "-l",
+        type=str,
+        required=False,
+        default="compression.log",
+        help="Directory to search for uncompressed/human readable sequence data.",
+    )
     args = parser.parse_args()
-    return args.search_dir
+    return args.search_dir, args.logging_file
 
 
 # Function to find files
@@ -27,13 +38,13 @@ def find_files(directory: str, extensions: Tuple[str, ...]) -> List[str]:
     """ """
 
     file_list: List[str] = []
-    
+
     for root, _, files in os.walk(directory):
         for file in files:
             if file.endswith(extensions) and not file.startswith("._"):
-                #ic(file)
+                # ic(file)
                 file_list.append(os.path.join(root, file))
-    
+
     return file_list
 
 
@@ -43,27 +54,35 @@ def process_file(
     extensions1: Tuple[str],
     extensions2: Tuple[str],
     extensions3: Tuple[str],
-    log: logging.Logger,
+    log_path: str,
 ) -> None:
     """ """
 
-    if file not in preprocessed_files:
-        if file.endswith(extensions1) or file.endswith(extensions2):
-            log.info(f"Compressing: {file}")
-            subprocess.run(["pigz", file], check=True)
-        elif file.endswith(extensions3):
-            log.info(f"Converting to BAM: {file}")
-            subprocess.run(
-                ["samtools", "view", "-b", "-o", f"{file}.bam", file], check=True
-            )
-        preprocessed_files.add("INFO:"+file)
+    if file in preprocessed_files:
+        return
+
+    if file.endswith(extensions1) or file.endswith(extensions2):
+        subprocess.run(["pigz", file], check=True)
+    elif file.endswith(extensions3):
+        subprocess.run(
+            ["samtools", "view", "-b", "-o", f"{file}.bam", file], check=True
+        )
+
+    lock_path = f"{log_path}.lock"
+    lock = FileLock(lock_path, timeout=60)
+    lock.acquire()
+    try:
+        with open(log_path, "a", encoding="utf-8") as log:
+            log.write(f"{file}\n")
+    finally:
+        lock.release()
 
 
 def main() -> None:
     """ """
 
     # Set the directory to start the search
-    base_directory = parse_command_line_args()
+    base_directory, log_path = parse_command_line_args()
 
     # Define file extensions
     fasta_extensions = tuple([".fasta", ".fa"])
@@ -77,21 +96,10 @@ def main() -> None:
             for line in log_file:
                 processed_files.add(line.strip())
 
-    # Logging setup
-    logging.basicConfig(filename="processing_log.txt", level=logging.INFO)
-    logger = logging.getLogger("file_processor")
-
     # Get lists of files
-    with open("file_list.txt", "a", encoding="utf-8") as list_handle:
-        fasta_files = find_files(base_directory, fasta_extensions)
-        fastq_files = list(find_files(base_directory, fastq_extensions))
-        sam_files = list(find_files(base_directory, sam_extensions))
-        for item in fasta_files:
-            list_handle.write(f"{item}\n")
-        for item in fastq_files:
-            list_handle.write(f"{item}\n")
-        for item in sam_files:
-            list_handle.write(f"{item}\n")
+    fasta_files = find_files(base_directory, fasta_extensions)
+    fastq_files = find_files(base_directory, fastq_extensions)
+    sam_files = find_files(base_directory, sam_extensions)
 
     # Create a pool of workers for parallel processing
     pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
@@ -106,15 +114,12 @@ def main() -> None:
                 fasta_extensions,
                 fastq_extensions,
                 sam_extensions,
-                logger,
+                log_path,
             )
             for file in fasta_files + fastq_files + sam_files
         ],
     )
-    
-    for file in fasta_files + fastq_files + sam_files:
-        logger.info(file)
-    
+
     # Close the pool
     pool.close()
     pool.join()
